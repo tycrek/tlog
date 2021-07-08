cb43195645eb21b1dd702c875ddee8fab28ac43c
tycrek <josh.moore@jmoore.dev>
2021-07-08 17:19:43 -0600

    added JSDoc

--- tlog.js
+++ tlog.js
@@ -1,5 +1,6 @@
 const { DateTime } = require('luxon');
 const chalk = require('chalk');
+const AvailableColours = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white', 'gray'];
 
 //#region Constants
 const CHARS = {
@@ -58,22 +59,50 @@ const LOG = {
 //#endregion
 
 class TLog {
+	/**
+	 * The colours users can set
+	 * @see {@link AvailableColours}
+	 * @readonly
+	 * @public
+	 * @static
+	 */
+	static AvailableColours = AvailableColours;
+
+	/**
+	 * Staticly exposed console, in case you wanted to use it
+	 * @readonly
+	 * @public
+	 * @static
+	 */
 	static console = console;
+
+	/**
+	 * Non-static exposed console, in case you wanted to call it from your TLog instance
+	 * @readonly
+	 * @public
+	 */
 	console = console;
 
+	/**
+	 * The options the logger will use
+	 * @type {object}
+	 * @private
+	 */
 	#options = {};
+
+	/**
+	 * Create a new instance of TLog
+	 * @param {object} {@link OPTIONS options} The options to use
+	 */
 	constructor(options = OPTIONS) {
 		this.#options = Object.assign({}, OPTIONS, options);
 	}
 
-	debug(title, message, extra) { return this.#log('debug', title, message, extra); }
-	info(title, message, extra) { return this.#log('info', title, message, extra); }
-	warn(title, message, extra) { return this.#log('warn', title, message, extra); }
-	error(title, message, extra) { return this.#log('error', title, message, extra); }
-	success(title, message, extra) { return this.#log('success', title, message, extra); }
-	comment(message) { console.log(chalk.grey(`${this.#options.comments} ${message}`)); return this; }
-	blank() { console.log(''); return this; }
-
+	/**
+	 * Generate a log-ready timestamp
+	 * @return {string} A timestamp, with colours & formatting (if enabled)
+	 * @private
+	 */
 	#getTimestamp() {
 		const timestamp = this.#options.timestamp;
 		return timestamp.enabled
@@ -86,6 +115,12 @@ class TLog {
 			: CHARS.EMPTY;
 	}
 
+	/**
+	 * Generate a log-ready label
+	 * @param {string} level The log level
+	 * @return {string} A label, with colours & formatting (if enabled)
+	 * @private
+	 */
 	#getLabel(level) {
 		const label = this.#options.label;
 		return label.enabled
@@ -93,18 +128,51 @@ class TLog {
 			: CHARS.EMPTY;
 	}
 
+	/**
+	 * Generate a log-ready title
+	 * @param {string} level The log level
+	 * @param {string} title The log title
+	 * @param {string} message The log message
+	 * @return {string} A title, with colours & formatting (if set)
+	 * @private
+	 */
 	#getTitle(level, title, message) {
 		return chalk[LOG.COLOURS[level]].bold(message ? `${title}${this.#options.title.delim}` : CHARS.EMPTY);
 	}
 
+	/**
+	 * Generate a log-ready message
+	 * @param {string} level The log level
+	 * @param {string} title The log title
+	 * @param {string} message The log message
+	 * @return {string} A message, with colours & formatting
+	 * @private
+	 */
 	#getMessage(level, title, message) {
 		return chalk[LOG.COLOURS[level]](message || title);
 	}
 
+	/**
+	 * Generate a log-ready extra
+	 * @param {string} level The log level
+	 * @param {string} extra The log extra
+	 * @return {string} An extra, with colours & formatting (if set)
+	 * @private
+	 */
 	#getExtra(level, extra) {
 		return chalk[LOG.COLOURS[level]].italic(extra ? `${this.#options.extra.prefix}${extra}${this.#options.extra.suffix}` : CHARS.EMPTY);
 	}
 
+	/**
+	 * Prints a formatted log message
+	 * @param {string} level The log level
+	 * @param {string} [title] The log title (optional)
+	 * @param {string} message The log message
+	 * @param {string} [extra] The log extra (optional)
+	 * @return {TLog} This instance of TLog
+	 * @private
+	 * @chainable
+	 */
 	#log(level, title, message, extra) {
 		console.log(
 			this.#getTimestamp() +
@@ -112,6 +180,92 @@ class TLog {
 			this.#getTitle(level, title, message) +
 			this.#getMessage(level, title, message) +
 			this.#getExtra(level, extra));
+		return this;
+	}
+
+	/**
+	 * Prints a formatted debug log message
+	 * @param {string} [title] The log title (optional)
+	 * @param {string} message The log message
+	 * @param {string} [extra] The log extra (optional)
+	 * @return {TLog} This instance of TLog
+	 * @public
+	 * @chainable
+	 */
+	debug(title, message, extra) {
+		return this.#log('debug', title, message, extra);
+	}
+
+	/**
+	 * Prints a formatted info log message
+	 * @param {string} [title] The log title (optional)
+	 * @param {string} message The log message
+	 * @param {string} [extra] The log extra (optional)
+	 * @return {TLog} This instance of TLog
+	 * @public
+	 * @chainable
+	 */
+	info(title, message, extra) {
+		return this.#log('info', title, message, extra);
+	}
+
+	/**
+	 * Prints a formatted warn log message
+	 * @param {string} [title] The log title (optional)
+	 * @param {string} message The log message
+	 * @param {string} [extra] The log extra (optional)
+	 * @return {TLog} This instance of TLog
+	 * @public
+	 * @chainable
+	 */
+	warn(title, message, extra) {
+		return this.#log('warn', title, message, extra);
+	}
+
+	/**
+	 * Prints a formatted error log message
+	 * @param {string} [title] The log title (optional)
+	 * @param {string} message The log message
+	 * @param {string} [extra] The log extra (optional)
+	 * @return {TLog} This instance of TLog
+	 * @public
+	 * @chainable
+	 */
+	error(title, message, extra) {
+		return this.#log('error', title, message, extra);
+	}
+
+	/**
+	 * Prints a formatted success log message
+	 * @param {string} [title] The log title (optional)
+	 * @param {string} message The log message
+	 * @param {string} [extra] The log extra (optional)
+	 * @return {TLog} This instance of TLog
+	 * @public
+	 * @chainable
+	 */
+	success(title, message, extra) { return this.#log('success', title, message, extra); }
+
+	/**
+	 * Prints a formatted comment log message
+	 * @param {string} message The log message
+	 * @return {TLog} This instance of TLog
+	 * @public
+	 * @chainable
+	 */
+	comment(message) {
+		console.log(chalk[this.#options.comments.colour](`${this.#options.comments.char} ${message}`));
+		return this;
+	}
+
+	/**
+	 * Prints a blank line
+	 * @return {TLog} This instance of TLog
+	 * @public
+	 * @chainable
+	 */
+	blank() {
+		console.log('');
 		return this;
 	}
 }
