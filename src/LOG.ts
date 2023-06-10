62a7ef6044b1560b84d585250ff1481d07f5c350
tycrek <gpg+github@jmoore.dev>
2023-06-10 17:50:24 -0600

    feat: migrated old LOG constants

---
+++ src/LOG.ts
@@ -0,0 +1,38 @@
+export const LOG = {
+	LEVELS: {
+		debug: 100,
+		info: 200,
+		warn: 300,
+		error: 400,
+
+		success: 200,
+		utils: 100,
+	},
+	COLOURS: {
+		debug: 'white',
+		info: 'cyan',
+		warn: 'yellow',
+		error: 'red',
+
+		success: 'green',
+		utils: 'grey',
+	},
+	TITLES: {
+		debug: '[DEBUG]',
+		info: '[INFO]',
+		warn: '[WARN]',
+		error: '[ERROR]',
+
+		success: '[SUCCESS]',
+		utils: '[UTILS]',
+	},
+	SPACES: {
+		debug: '   ',
+		info: '    ',
+		warn: '    ',
+		error: '   ',
+
+		success: ' ',
+		utils: '   ',
+	}
+};
