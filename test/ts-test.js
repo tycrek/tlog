9a88995223d72f5c743fca98ccdf52ffac2fb613
tycrek <gpg+github@jmoore.dev>
2023-06-10 23:19:55 -0600

    test: updated tests

--- test/ts-test.js
+++ test/ts-test.js
@@ -1,10 +1,32 @@
 const { TLog } = require('..');
 
-const what = 'what';
+const log = new TLog('debug');
 
-const tlog = new TLog('info');
-tlog.debug('debug', what);
-tlog.info('info', what);
-tlog.warn('warn', what);
-tlog.error('error', what);
-tlog.fatal('fatal', what);
+log.debug('Simple debug message')
+	.info('Simple info message')
+	.warn('Simple warn message')
+	.error('Simple error message')
+	.fatal('Simple fatal message')
+	.success('Simple success message')
+	.utils('Simple utils message');
+
+log.debug('Debug message with a:', 'Title')
+	.info('Info message with a:', 'Title')
+	.warn('Warn message with a:', 'Title')
+	.error('Error message with a:', 'Title')
+	.fatal('Fatal message with a:', 'Title')
+	.success('Success message with a:', 'Title')
+	.utils('Utils message with a:', 'Title');
+
+log.debug('Debug message with a:', 'Title', 'and extra')
+	.info('Info message with a:', 'Title', 'and extra')
+	.warn('Warn message with a:', 'Title', 'and extra')
+	.error('Error message with a:', 'Title', 'and extra')
+	.fatal('Fatal message with a:', 'Title', 'and extra')
+	.success('Success message with a:', 'Title', 'and extra')
+	.utils('Utils message with a:', 'Title', 'and extra');
+
+log.comment('This is a test comment.', 'Here is another argument.')
+	.basic('This is a test basic.', 'Here is another argument.')
+	.blank()
+	.comment('That was a blank line.');
