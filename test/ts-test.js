7a087a495ee5b96b36e5a9b188ff110afc54da7f
tycrek <gpg+github@jmoore.dev>
2023-06-12 16:46:10 -0600

    fix: test titles

--- test/ts-test.js
+++ test/ts-test.js
@@ -10,21 +10,21 @@ log.debug('Simple debug message')
 	.success('Simple success message')
 	.utils('Simple utils message');
 
-log.debug('Debug message with a:', 'Title')
-	.info('Info message with a:', 'Title')
-	.warn('Warn message with a:', 'Title')
-	.error('Error message with a:', 'Title')
-	.fatal('Fatal message with a:', 'Title')
-	.success('Success message with a:', 'Title')
-	.utils('Utils message with a:', 'Title');
+log.debug('Debug message with a', 'Title')
+	.info('Info message with a', 'Title')
+	.warn('Warn message with a', 'Title')
+	.error('Error message with a', 'Title')
+	.fatal('Fatal message with a', 'Title')
+	.success('Success message with a', 'Title')
+	.utils('Utils message with a', 'Title');
 
-log.debug('Debug message with a:', 'Title', 'and extra')
-	.info('Info message with a:', 'Title', 'and extra')
-	.warn('Warn message with a:', 'Title', 'and extra')
-	.error('Error message with a:', 'Title', 'and extra')
-	.fatal('Fatal message with a:', 'Title', 'and extra')
-	.success('Success message with a:', 'Title', 'and extra')
-	.utils('Utils message with a:', 'Title', 'and extra');
+log.debug('Debug message with a', 'Title', 'and extra')
+	.info('Info message with a', 'Title', 'and extra')
+	.warn('Warn message with a', 'Title', 'and extra')
+	.error('Error message with a', 'Title', 'and extra')
+	.fatal('Fatal message with a', 'Title', 'and extra')
+	.success('Success message with a', 'Title', 'and extra')
+	.utils('Utils message with a', 'Title', 'and extra');
 
 log.comment('This is a test comment.', 'Here is another argument.')
 	.basic('This is a test basic.', 'Here is another argument.')
